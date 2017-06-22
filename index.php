9ca908fbfddf7eb2cc28bd0aaab2cbdc4277afe5
Wallyson Carvalho <wallyson.carvalho@live.com>
2017-06-22 10:12:49 -0300

    Comeco do site em php

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php include 'paginas/header.php'; ?>
+
+         <!-- comeco da seção de anuncio -->
+        <section id="home" class="home">
+            <div class="home_overlay">
+                <div class="container">
+                    <div class="row">
+					
+						<div class="col-sm-12 col-sm">
+							<div class="main_home">
+								<div class="mainhomecontent">
+									<div class="single_home">
+										<div id="destaque" class="carousel slide" data-ride="carousel">
+										  <!-- Indicators -->
+										  <ol class="carousel-indicators">
+										    <li data-target="#destaque" data-slide-to="0" class="active"></li>
+										    <li data-target="#destaque" data-slide-to="1"></li>
+										    <li data-target="#destaque" data-slide-to="2"></li>
+										  </ol>
+
+										  <!-- Wrapper for slides -->
+										  <div class="carousel-inner">
+										    <div class="item active">
+										      <img src="assets/images/logoCA2.png" alt="Centro Acadêmico de Ciência da Computação">
+										    </div>
+
+										    <div class="item">
+										      <div class="carousel-content">
+										      	<div id="text-carousel">
+										      		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										      		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										      		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+										      		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+										      		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+										      		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+										      	</div>
+										      </div>
+										    </div>
+										  </div>
+
+										  <!-- Left and right controls -->
+										  <a class="left carousel-control" href="#destaque" data-slide="prev">
+										    <span class="glyphicon glyphicon-chevron-left"></span>
+										    <span class="sr-only">Anterior</span>
+										  </a>
+										  <a class="right carousel-control" href="#destaque" data-slide="next">
+										    <span class="glyphicon glyphicon-chevron-right"></span>
+										    <span class="sr-only">Proximo</span>
+										  </a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+						
+                </div>
+            </div>
+        </section>
+        <!-- fim da seção de anuncio -->
+
+
+<?php include 'paginas/footer.php'; ?>
