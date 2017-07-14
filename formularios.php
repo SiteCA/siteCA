7553a605346197688963b72ac9e29c9dd97e9539
LeandroNog <leandronog.ufes@gmail.com>
2017-07-14 10:39:17 -0300

    Correção do texto - Aba Formularios

--- formularios.php
+++ formularios.php
@@ -10,15 +10,15 @@
 				<a href="Formularios_e_Documentos/Documentos_Curso/CC.PPC - Completo.pdf">PPC do Curso</a><br/>
 				<a href="Formularios_e_Documentos/Documentos_Curso/grade_CienComp_Correcao.pdf">Grade Curricular</a><br/>
 
-				<br/><p> Estagio Obrigatorio e nao obrigatorio </p>
-				<a href="Formularios_e_Documentos/Estagio_Obrigatorio_e_Nao_Obrigatorio/formulario_de_solicitacao_de_certificado.pdf">Formulario de Solicitacao</a><br/>
+				<br/><p> Estagio Obrigatório e nao obrigatório </p>
+				<a href="Formularios_e_Documentos/Estagio_Obrigatorio_e_Nao_Obrigatorio/formulario_de_solicitacao_de_certificado.pdf">Formulário de Solicitação</a><br/>
 				<a href="Formularios_e_Documentos/Estagio_Obrigatorio_e_Nao_Obrigatorio/Modelo_Plano_Trabalho_em_Branco.pdf">Modelo/Plano</a><br/>
 				<a href="Formularios_e_Documentos/Estagio_Obrigatorio_e_Nao_Obrigatorio/termo_aditivo_revisado.pdf">Termo Aditivo Revisado</a><br/>
 				<a href="Formularios_e_Documentos/Estagio_Obrigatorio_e_Nao_Obrigatorio/TERMO_DE_COMPROMISSO_ESTÁGIO_INTERNO_UFES_DIGITAVEL.pdf">Termo de compromisso estágio interno</a><br/>
 				<a href="Formularios_e_Documentos/Estagio_Obrigatorio_e_Nao_Obrigatorio/Termo_de_Compromisso_Estagio_Nao_Obrigatorio.pdf">Termo de compromisso estágio não obrigatório</a><br/>
 
 
-				<br/><p>Formularios Sugrad</p>
+				<br/><p>Formulários Sugrad</p>
 				<a href="Formularios_e_Documentos/Formularios_Sugrad/AJUDA_DE_CUSTO.pdf">Ajuda de custo</a><br/>
 				<a href="Formularios_e_Documentos/Formularios_Sugrad/FORMULARIO_PROGRAMA_DE_ATIVIDADES_COMPLEMENTARES_0.pdf">Atividades complementares</a><br/>
 				<a href="Formularios_e_Documentos/Formularios_Sugrad/Instrucao_Normativa 003-2014_ANEXO_I.pdf">Requerimento de antecipação de colação de grau</a><br/>
